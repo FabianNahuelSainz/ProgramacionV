24c6565d510c2d2babf4d4c891c92a064cf92b67
FabianNahuelSainz <fabiansainz95@gmail.com>
2025-04-01 14:06:52 -0300

    Se agrega el contenido de la semana 2

---
+++ Controllers/HelloWorldController.cs
@@ -0,0 +1,29 @@
+﻿using System.Text.Encodings.Web;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public string Index()
+        {
+            return "This is my default action...";
+        }
+
+        public string Welcome()
+        {
+            return "This is the Welcome action method...";
+        }
+
+        public string Bienvenido(string name, int numTimes = 1)
+        {
+            return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
+        }
+
+        public string BienvenidoId(string name, int ID = 1)
+        {
+            return HtmlEncoder.Default.Encode($"Hello {name}, ID: {ID}");
+        }
+
+    }
+}

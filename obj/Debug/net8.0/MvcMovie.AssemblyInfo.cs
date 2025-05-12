edc562fc041fe4ba29e923ff9d385266a6d80d5f
FabianNahuelSainz <fabiansainz95@gmail.com>
2025-05-12 00:34:30 -0300

    Se agrega actividad semana 7

--- obj/Debug/net8.0/MvcMovie.AssemblyInfo.cs
+++ obj/Debug/net8.0/MvcMovie.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6c80d3b88d08fa8890f4f3d296df8b38926facc1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+33ff3be7e935c1cbde8150ec4c9b3a507bcfa26a")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

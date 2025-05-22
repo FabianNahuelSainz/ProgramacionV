b6212bf01fcea384cdfd308c80161789bb66f398
FabianNahuelSainz <fabiansainz95@gmail.com>
2025-05-22 16:34:52 -0300

    Actividad semana 8

---
+++ Controllers/UsuarioController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcMovie.Models;
+
+namespace MvcMovie.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ForMetodo()
+        {
+            Usuario usuario;
+            List<Usuario> listaUsuario = new List<Usuario>();
+
+            for (int i = 0; i <= 4; i++)
+            {
+                usuario = new Usuario();
+                usuario.Id = i;
+                usuario.Nombre = "Fabian";
+                usuario.Apellido = "Sainz";
+                usuario.Email = "FabianSainz@gmail.com";
+                usuario.Username = "fsainz";
+                usuario.Password = "1234";
+
+                listaUsuario.Add(usuario);
+
+            }
+            ViewBag.ListaUsuario=listaUsuario;
+
+            return View("Index");
+        }
+    }
+}
--- obj/Debug/net8.0/MvcMovie.AssemblyInfo.cs
+++ obj/Debug/net8.0/MvcMovie.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+33ff3be7e935c1cbde8150ec4c9b3a507bcfa26a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+edc562fc041fe4ba29e923ff9d385266a6d80d5f")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
